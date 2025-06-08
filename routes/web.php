20b2fd22d914aa83bc470bf98762d650534147de
Muhammad Khairul Iqbal <muhammakhairul22@gmail.com>
2025-06-08 22:53:30 +0700

    feat: Membuat fitur manajemen berita

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KodeController;
-use App\Http\Controllers\LokasiController;    
+use App\Http\Controllers\LokasiController;
+use App\Http\Controllers\BeritaController;    
 
 Route::get('/', function () {
     return view('welcome');
@@ -53,3 +54,12 @@ Route::post('/lokasi', [LokasiController::class, 'store'])->name('lokasi.store')
 Route::get('/lokasi/{lokasi}/edit', [LokasiController::class, 'edit'])->name('lokasi.edit');
 Route::put('/lokasi/{lokasi}', [LokasiController::class, 'update'])->name('lokasi.update');
 Route::delete('/lokasi/{lokasi}', [LokasiController::class, 'destroy'])->name('lokasi.destroy');
+
+// Tabel Berita
+Route::get('/berita', [BeritaController::class, 'index'])->name('berita.index');
+Route::get('/berita/create', [BeritaController::class, 'create'])->name('berita.create');
+Route::post('/berita', [BeritaController::class, 'store'])->name('berita.store');
+Route::get('/berita/{berita}', [BeritaController::class, 'show'])->name('berita.show');
+Route::get('/berita/{berita}/edit', [BeritaController::class, 'edit'])->name('berita.edit');
+Route::put('/berita/{berita}', [BeritaController::class, 'update'])->name('berita.update');
+Route::delete('/berita/{berita}', [BeritaController::class, 'destroy'])->name('berita.destroy');
